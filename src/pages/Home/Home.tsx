805d123d3d42717aa965f86523077c3205783553
Rohit Verma <Rohit.verma272727@gmail.com>
2024-07-30 10:59:44 +0000

    feat: Added coming soon in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,21 @@
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
-  return <h1>Home</h1>;
+  const navigate = useNavigate();
+  return (
+    <div className="d-flex items-center justify-content-center w-full py-5">
+      <div className="text-center">
+        <div className="mb-4 fs-2">Coming Soon...</div>
+        <Button
+          variant="primary"
+          onClick={() => navigate("/store")}
+        >
+          Check out Store
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
